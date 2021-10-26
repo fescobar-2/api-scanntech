33756e81b565c5ca37a2a57d2e12eaf8b74ff24d
Fede Escobar <fescobar_2@hotmail.com>
2021-10-26 10:13:26 -0300

    Added app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-    res.send('Hello World!');
-});
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+  })
 
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
